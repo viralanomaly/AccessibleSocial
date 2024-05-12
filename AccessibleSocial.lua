b63f310f5de1ee6bbc324ecf82e2404ea0887fd5
Sheila Kelly <36901025+viralanomaly@users.noreply.github.com>
2024-05-11 20:22:59 -0500

    Sturdiness events and group invites

--- AccessibleSocial.lua
+++ AccessibleSocial.lua
@@ -8,7 +8,13 @@ local REPLAY = "replay"
 local GUILD = "guild"
 local ONLINE = "online"
 local OFFLINE = "offline"
+local INVITEBNET = "invitebnet"
+local LISTBNET = "listbnet"
+local INVITEGUILDIE = "inviteguildie"
+
 local friends = {}
+local bnetFriendList = {}
+local guildieList = {}
 
 AccessibleSocial = LibStub("AceAddon-3.0"):NewAddon("AccessibleSocial", "AceConsole-3.0", "AceEvent-3.0")
 AS = AccessibleSocial
@@ -18,6 +24,26 @@ AS = AccessibleSocial
 local function IsPrintEnabled()
     local enablement = ASDB ~= nil and ASDB.enabled == true
     return enablement
+end
+
+local function GetBnetClientDesc(client)
+    local desc = "Unknown Client"
+    if client == BNET_CLIENT_WOW then 
+        desc = "World of Warcraft"
+    elseif client == BNET_CLIENT_APP then 
+        desc = "Battle.net desktop app"
+    elseif client == BNET_CLIENT_HEROES then 
+        desc = "Heroes of the Storm"
+    elseif client == BNET_CLIENT_CLNT then 
+        desc = "CLNT"
+    elseif client == "BSAp" then
+        desc = "Blizzard Services (Mobile)"
+    elseif client == "App" then
+        desc = "Blizzard Services (App)"
+    end
+
+    return desc
+    
 end
 
 --- Our custom print function.  Will print to screen and check the addon voice setting before concatenating the strings to text-to-speech.
@@ -52,7 +78,7 @@ end
 local function PrintFriend(accountInfo)
     if (IsPrintEnabled() and accountInfo ~= nil) then
             local battleTag = accountInfo.battleTag
-            local isOnline = accountInfo.gameAccountInfo.isOnline
+            local isOnline = accountInfo.gameAccountInfo.isOnline or (accountInfo.lastOnlineTime == nil)
             local characterName = accountInfo.gameAccountInfo.characterName
             local skipPrint = false
 
@@ -84,18 +110,72 @@ end
 
 function PrintGuild()
     local numTotalGuildMembers, numOnlineGuildMembers, _ = GetNumGuildMembers()
+    guildieList = {}
 
     AccessiblePrintWithPrefix(numOnlineGuildMembers.." guildies online out of", numTotalGuildMembers.." total members")
+    local onlineCount = 0
     for gIndex=1, numTotalGuildMembers do
         local name, rankName, _, level, classDisplayName, zone, _, _, isOnline, _ = GetGuildRosterInfo(gIndex)
         if(isOnline) then
-            PrintGuildie(name, rankName, level, classDisplayName, zone)
+            onlineCount = onlineCount + 1
+            PrintGuildie(onlineCount, name, rankName, level, classDisplayName, zone)
         end
     end
 end
 
-function PrintGuildie(name, rankName, level, classDisplayName, zone)
-    AccessiblePrint(rankName.." "..name, "level " .. level.." "..classDisplayName.." location: "..zone)
+function PrintGuildie(index, name, rankName, level, classDisplayName, zone)
+    guildieList[index] = name
+    AccessiblePrint("Guildie "..index..": "..rankName.." "..name, "level " .. level.." "..classDisplayName.." location: "..zone)
+end
+
+function ListBnetFriends()
+    bnetFriendList = {}
+
+    local total, online = BNGetNumFriends()
+    AccessiblePrint("You have "..total.." Battle.net friends and", online.." of them are online!")
+    local onlineIndex = 1
+    
+    for bIndex=1, total do
+        local friend = C_BattleNet.GetFriendAccountInfo(bIndex)
+        bnetFriendList[bIndex] = friend
+    end
+
+
+    for fIndex=1, total do
+        local indexString = "Friend "..fIndex
+        local friend = bnetFriendList[fIndex]
+        local descString = ""
+        local isOnline = friend.gameAccountInfo.isOnline or (friend.lastOnlineTime == nil)
+            
+        if(friend.gameAccountInfo ~= nil and isOnline and friend.gameAccountInfo.clientProgram ~= nil) then
+            descString = friend.battleTag.." is online in "..GetBnetClientDesc(friend.gameAccountInfo.clientProgram)
+            if(friend.gameAccountInfo.characterName ~= nil) then
+                descString = descString.." on toon "..friend.gameAccountInfo.characterName
+            end
+        elseif isOnline then
+            descString = friend.battleTag.." is online"
+        else
+            descString = friend.battleTag.." is offline"
+        end
+        AccessiblePrint(indexString, descString)
+    end
+end
+
+function InviteFriend(friendIndex)
+    local friend = bnetFriendList[tonumber(friendIndex)]
+    if(friend ~= nil and friend.gameAccountInfo ~= nil and friend.gameAccountInfo.clientProgram == BNET_CLIENT_WOW) then
+        local friendName = friend.gameAccountInfo.characterName.."-"..friend.gameAccountInfo.realmName
+        AccessiblePrint("About to invite", friendName)
+        C_PartyInfo.InviteUnit(friendName)
+    end
+end
+
+function InviteGuildie(guildieIndex)
+    local guildie = guildieList[tonumber(guildieIndex)]
+    if(guildie ~= nil) then
+        AccessiblePrint("About to invite", guildie)
+        C_PartyInfo.InviteUnit(guildie)
+    end
 end
 
 function AS:OnInitialize()
@@ -103,6 +183,7 @@ function AS:OnInitialize()
 
     local defaults = {
         enabled = true,
+        sturdiness = 100
     }
 
     if (ASDB == nil) then
@@ -118,6 +199,7 @@ function AS:OnEnable()
     AS:RegisterEvent("AUTOFOLLOW_BEGIN")
     AS:RegisterEvent("AUTOFOLLOW_END")
     AS:RegisterEvent("BN_FRIEND_INFO_CHANGED")
+    AS:RegisterEvent("STURDINESS_UPDATE")
     -- f:RegisterEvent("CHAT_MSG_BN_WHISPER")
     -- f:RegisterEvent("CHAT_MSG_INSTANCE_CHAT")
     -- f:RegisterEvent("CHAT_MSG_RAID")
@@ -127,6 +209,8 @@ function AS:OnEnable()
     -- f:RegisterEvent("CHAT_MSG_PARTY")
     -- Send on BN whisper, read it back
     -- Read who is doing what on battlenet
+    -- Send a message
+    -- invite to group
 
     PrintAddonStatus()
 end
@@ -146,9 +230,32 @@ function AS:AUTOFOLLOW_END(event)
     AccessiblePrintWithPrefix("No longer Autofollowing", "anyone")
 end
 
+function AS:STURDINESS_UPDATE(event)
+    local currentSturdiness = GetSturdiness()
+    local durability = "Durability is now " .. currentSturdiness
+    if ASDB.sturdiness ~= nil then
+        if(ASDB.sturdiness < 10) then
+            ASDB.sturdiness = currentSturdiness
+            AccessiblePrint("Repair now.", durability)
+        elseif (ASDB.sturdiness < 50 and ASDB.sturdiness > (currentSturdiness + 5)) then
+            ASDB.sturdiness = currentSturdiness
+            AccessiblePrint("Repair soon.", durability)
+        elseif(ASDB.sturdiness > (currentSturdiness + 9)) then
+            ASDB.sturdiness = currentSturdiness
+            AccessiblePrint("Don't forget to repair.", durability)
+        end
+    else
+        ASDB.sturdiness = currentSturdiness
+    end
+
+end
+
 -- Configure slash commands for enable and disable of the printing
 function AS:SlashCommand(msg)
-    local cmd1 = strsplit(" ", msg)
+    local cmd1, cmd2 = strsplit(" ", msg)
+    local orAS = " or /as "
+    local help1 = "Enter a command: /as "..ENABLE..orAS..DISABLE..orAS..GUILD..orAS..LISTBNET
+    local help2 = orAS..INVITEBNET.." followed by the number from "..LISTBNET.."for that friend"..orAS..INVITEGUILDIE.." followed by the number from "..GUILD.." for that guild member"
 
     if #cmd1 > 0 then
         cmd1 = strlower(cmd1)
@@ -161,9 +268,17 @@ function AS:SlashCommand(msg)
             PrintAddonStatus()
         elseif cmd1 == GUILD then
             PrintGuild()
+        elseif cmd1 == INVITEBNET then
+            InviteFriend(cmd2)
+        elseif cmd1 == LISTBNET then
+            ListBnetFriends()
+        elseif cmd1 == INVITEGUILDIE then
+            InviteGuildie(cmd2)
+        else
+            AccessiblePrintWithPrefix(help1, help2)
         end
     else
-        AccessiblePrintWithPrefix("Enter a command: /as "..ENABLE.." or /as "..DISABLE, " or /as "..VOICE.." or /as "..NOVOICE.." or /as "..GUILD)
+        AccessiblePrintWithPrefix(help1, help2)
     end
 end
 
